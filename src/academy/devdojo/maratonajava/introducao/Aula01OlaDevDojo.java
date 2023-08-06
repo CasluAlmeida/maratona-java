1446175509593f4c3e00ead6f2620069183775a8
Lucas19Almeida <almeidalucas788@gmail.com>
2023-08-06 15:48:19 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Aula de Comentarios
+ */
+
+/*
+    Tipos de comentarios
+ */
+
+// Comentarios
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo");
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 10;
+        long nomeroGrande = 1000000;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        float peso = 128.8F;
+        double pesoDouble = 128.8;
+        boolean verdadeido = true;
+        boolean falso = false;
+        char sexoMasculino = 'M';
+        char sexoFeminino = 'F';
+        char sexoA = '\u0041';
+
+        System.out.println("A idade é "+idade+" anos!");
+        System.out.println(sexoMasculino);
+        System.out.println(sexoFeminino);
+        System.out.println(sexoA);
+
+    }
+}
